03a19c53fbef0cc3c31e3d4de6866907ec7b2ab6
munxw_ <momunov.kr@gmail.com>
2023-02-17 11:39:37 +0600

    verifPage all rigth

--- src/components/Verification/Verification.jsx
+++ src/components/Verification/Verification.jsx
@@ -10,13 +10,40 @@ const Verification = () => {
       <div className="background">
         <Container>
           <div className="verif_display">
-            {/* <p className="verif_p">
+            {/* ! EnterEmailPage ------------------------------------------------------------------------*/}
+            {/*
+            <p className="verif_p">
               We have sent a temporary verification code to chto-to@gmail.com.
               Enter the code to verify this device
             </p>
 
-            <p className="verif_codeP">Enter Verification Code:</p>
+             <p className="verif_p">
+                Enter your email address to recover lost password
+              </p>
+              
+              <p className="verif_codeP">Enter Email address:</p>
+              
+              <TextField
+                label="Email"
+                variant="outlined"
+                color="info"
+                style={{
+                  width: "400px",
+                  backgroundColor: "rgba(255, 255, 255, 0.5)",
+                  borderRadius: "5px",
+                }}
+              ></TextField>
+              
+              <button className="verif_btn">VERIFY</button>*/}
+            {/* ? EnterEmailPage  END ------------------------------------------------------------------------*/}
 
+            {/* ! EnterCode ------------------------------------------------------------------------*/}
+
+            <p className="verif_p">
+              We have sent a temporary verification code to chto-to@gmail.com.
+              Enter the code to verify this device
+            </p>
+            <p className="verif_codeP">Enter Verification Code:</p>
             <div className="verif_code_Ablock">
               <div className="verif_code__blocks"></div>
               <div className="verif_code__blocks a"></div>
@@ -29,27 +56,35 @@ const Verification = () => {
               <div className="verif_code__blocks a"></div>
               <div className="verif_code__blocks a"></div>
             </div>
+            {/* ! EnterCode  END ------------------------------------------------------------------------*/}
 
-            <button className="verif_btn">VERIFY</button> */}
-
-            <p className="verif_p">
-              Enter your email address to recover lost password
-            </p>
-
-            <p className="verif_codeP">Enter Email address:</p>
+            {/* ! Enter new Password ------------------------------------------------------------------------*/}
+            <p className="verif_codeP">Enter password</p>
 
             <TextField
-              label="Email"
+              label="Password"
               variant="outlined"
               color="info"
               style={{
-                width: "400px",
+                width: "30vw",
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                borderRadius: "5px",
+                marginBottom: "15px",
+              }}
+            ></TextField>
+            <TextField
+              label="confirm Password"
+              variant="outlined"
+              color="info"
+              style={{
+                width: "30vw",
                 backgroundColor: "rgba(255, 255, 255, 0.5)",
                 borderRadius: "5px",
               }}
             ></TextField>
 
-            <button className="verif_btn">VERIFY</button>
+            <button className="verif_btn">NEXT</button>
+            {/* ! Enter new Password  END ------------------------------------------------------------------------*/}
           </div>
         </Container>
       </div>
